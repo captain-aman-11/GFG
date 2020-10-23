5f3992b709e8df3f13fc06e5a4f953baf0101d12
Aman <aakashaman1770@gmail.com>
2020-10-23 12:37:09 +0530

    Added files

--- Programming Languages/C++2/Vectors/Vector_Modifiers.cpp
+++ Programming Languages/C++2/Vectors/Vector_Modifiers.cpp
@@ -21,19 +21,72 @@ using namespace std;
 
 int main() {
   //Assign vector
-  vector<int> v1;
+  vector<int> v;
 
   // fill the array with 10 five times
-  v1.assign(5,10);
+  v.assign(5,10);
 
   cout << "The vector elements are: ";
-  for(int i=0; i < v1.size(); i++)
-  cout << v1[i] << " ";
+  for(int i=0; i < v.size(); i++)
+  cout << v[i] << " ";
 
   //inserts 15 to the last position
-  v1.push_back(15);
-  int n = v1.size();
-  cout << "\nThe last element is : "<< v1[n-1];
+  v.push_back(15);
+  int n = v.size();
+  cout << "\nThe last element is : "<< v[n-1];
 
+  //removes last element
+  v.pop_back();
 
+  // prints the vector
+  cout << "\nTHe vector elements are: ";
+  for(int i=0; i < v.size(); i++)
+  cout << v[i] << " ";
+
+  //Inserts 5 at the beginning
+  v.insert(v.begin(), 5);
+  cout << "\n The first element is : " << v[0];
+
+  //removes the first elements
+  v.erase(v.begin());
+  cout << "\n The first element is : " << v[0];
+
+  //Inserts at the beginning
+  v.emplace(v.begin(), 5);
+  cout << "\n The first element is : " << v[0];
+
+  // Inserts 20 at the end
+  v.emplace_back(20);
+  n = v.size();
+  cout << "\nThe last element is : " << v[n-1];
+
+// erases the vector
+v.clear();
+cout << "\nVector size after erase(): " << v.size();
+
+//two vector to perform swap
+vector<int> v1,v2;
+v1.push_back(1);
+v1.push_back(2);
+v2.push_back(3);
+v2.push_back(4);
+
+cout << "\n\nVector 1: ";
+for(int i=0; i < v1.size(); i++)
+  cout << v1[i] << " ";
+
+cout << "\nVector 2: ";
+for(int i=0; i < v2.size(); i++)
+    cout << v2[i] << " ";
+
+//Swaps v1 and v2
+v1.swap(v2);
+
+cout << "\nAfter Swap \n\nVector 1: ";
+for(int i=0; i < v1.size(); i++)
+cout << v1[i] << " ";
+
+cout << "\nVector 2: ";
+for(int i=0; i < v2.size(); i++)
+cout << v2[i] << " ";
 }
