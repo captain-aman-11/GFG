fa3edc6299416cc889a6f33887d8347a3d6aad95
Aman <aakashaman1770@gmail.com>
2020-10-23 09:22:05 +0530

    Added files

--- Programming Languages/C++2/Vectors/vector1.cpp
+++ Programming Languages/C++2/Vectors/vector1.cpp
@@ -2,9 +2,18 @@
 #include <iostream>
 using namespace std;
 
+// int main(){
+//   int n =10;
+//   vector<int> v(n,5); // It will create a vector of n size in which all the elements contains 5
+//   for(auto x : v){
+//     cout << x << " ";
+//   }
+//   return 0;
+// }
+
+//Intializing a vector
 int main(){
-  int n =10;
-  vector<int> v(n,5); // It will create a vector of n size in which all the elements contain 5
+  vector<int> v {10,20,30,40,50};
   for(auto x : v){
     cout << x << " ";
   }
