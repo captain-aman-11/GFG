d9e43cbbdbf9549cbcd8beec3d17df8d39b33506
Aman <aakashaman1770@gmail.com>
2020-11-02 15:52:16 +0530

    Added files

---
+++ Programming Languages/C++2/Arrays/Array Operations/Largest_element.cpp
@@ -0,0 +1,26 @@
+//Largest element in an Array
+
+#include <iostream>
+using namespace std;
+
+int LargestElement(int arr[], int arrSize){
+  int largest = arr[0];
+  for(int i=1; i < arrSize; i++)
+{
+  if(arr[i] > largest)
+  largest = arr[i];
+}
+for(int i=0; i < arrSize;i++){
+  if(arr[i] == largest){
+    return i;
+  }
+}
+}
+
+int main(){
+  int arrSize =5;
+  int arr[] = {2, 5,100, 15,22};
+  int ans = LargestElement(arr,arrSize);
+  cout <<"Ïndex of the largest elemnt is : " << ans;
+  return 0;
+}
