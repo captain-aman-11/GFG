e3746096c10f5569ac1b3b9062b185d4953b0926
Aman <aakashaman1770@gmail.com>
2020-11-02 14:52:43 +0530

    Added files

---
+++ Programming Languages/C++2/Arrays/Array Operations/Insert.cpp
@@ -0,0 +1,30 @@
+//Inserting in an Array
+
+#include <iostream>
+using namespace std;
+
+int insert(int arr[], int noToInsert, int arrSize, int capacity, int position ){
+  if(arrSize == capacity)
+  return arrSize; //When the array is already full
+
+  int idx = position -1; // Index where the element need to be inserted
+
+  for(int i=arrSize-1; i>=idx; i--)
+  arr[i+1] = arr[i]; //Shifting all elements one block ahead
+
+  arr[idx] = noToInsert;
+  return arrSize+1;
+}
+
+int main(){
+  int capacity =5;
+  int arr[capacity] = {5,10,20}; // 2 space is vacant
+  int arrSize = 3; //Three elements are filled till now
+  int noToInsert = 7;
+  int position = 2;
+  insert(arr,noToInsert,arrSize,capacity,position);
+  for(int i=0; i < capacity ; i++){
+    cout << arr[i] << endl;
+  }
+  return 0;
+}
---
+++ Programming Languages/C++2/Arrays/Array Operations/Linear_search.cpp
@@ -0,0 +1,19 @@
+// Searching in an Array
+
+#include <iostream>
+using namespace std;
+
+int search(int arr[], int arrSize,int x){
+  for(int i=0; i < arrSize; i++)
+  if(arr[i] == x)
+  return i;
+
+  return -1;
+}
+
+int main(){
+  int x = 17; // Number needed to be searched
+  int arr[] = {20,5,7,25};
+  int arrSize = 4;
+  cout << "Index is : " << search(arr,4,x);
+}
