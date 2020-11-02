bf2bebb67c022ce90ca957ea52db7c24ec327c94
Aman <aakashaman1770@gmail.com>
2020-11-02 14:39:50 +0530

    Added files

--- Programming Languages/C++2/Exception Handling/Try_Throw_Catch1.cpp
+++ Programming Languages/C++2/Exception Handling/Try_Throw_Catch1.cpp
@@ -21,14 +21,14 @@ try{
   if(y==0){
     throw 0;
   }
-  if(y==1){
+  if(y==1){ // To check for multiple catch blocks
     throw 1;
   }
   cout << "Result is : " << x/y;
 }
 
 catch(int x){ //We are throwing an integer 0, that's why here is int x.
-  cout << "Division is :" << x;
+  cout << "Division is : " << x;
 }
 return 0;
 }
