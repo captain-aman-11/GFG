fac5731e155b31cefe608548ef1f3f1fbbf1a1eb
Aman <aakashaman1770@gmail.com>
2020-11-09 09:00:36 +0530

    Added Files

--- Programming Languages/C++2/Demo.cpp
+++ Programming Languages/C++2/Demo.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 int main(){
-  int arr[] = {10,20,5,7};
-  int n = sizeof(arr)/sizeof(arr[0]);
-  sort(arr,arr+n);
-
-  for(int x: arr)
-    cout << x << " ";
-
-  sort(arr,arr + n,greater<int>);
-  for(int x: arr)
-    cout << x << " ";
-
-  return 0;
+  for(int i=0; i < 1000000000; i++)
+    cout << i << endl;
 }
