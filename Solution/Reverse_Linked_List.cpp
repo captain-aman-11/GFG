233f2b53e3e8214d7f694a4af7d09a87133dbb85
Aman <aakashaman1770@gmail.com>
2020-12-08 07:19:31 +0530

    Added Files

--- Solution/Reverse_Linked_List.cpp
+++ Solution/Reverse_Linked_List.cpp
@@ -2,6 +2,7 @@
 
 // Reverse Linked List using Recursion
 
+//CPP
 class Solution
 {
 public
@@ -15,6 +16,35 @@ public
 
         head->next->next = head;
         head->next = NULL;
+
+        return rest;
+    }
+}
+
+//Java
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution
+{
+public
+    ListNode reverseList(ListNode head)
+    {
+        if (head == null || head.next == null)
+        {
+            return head;
+        }
+        ListNode rest = reverseList(head.next);
+
+        head.next.next = head;
+        head.next = null;
 
         return rest;
     }
