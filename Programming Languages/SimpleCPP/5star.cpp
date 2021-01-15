892fb0caa2efa03a55cb3981d1f08df4370c32ac
Aman <aakashaman1770@gmail.com>
2021-01-15 17:30:52 +0530

    Added SimpleCPP files

--- Programming Languages/SimpleCPP/5star.cpp
+++ Programming Languages/SimpleCPP/5star.cpp
@@ -4,8 +4,8 @@ main_program{
     turtleSim();
     repeat(5){
         forward(100);
-        right(135);
-        wait(5);
+        right(144); // Since angle between lines of star is 36 degree. Hence angle to move = 90 + (90 - 36) = 90 + 54 = 144
+        wait(0.3);
     }
-    wait(50);
+    wait(50999);
 }
