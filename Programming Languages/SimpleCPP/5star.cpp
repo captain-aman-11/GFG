3b3bcd5765491e14bab91f8810152a6e638036b8
Aman <aakashaman1770@gmail.com>
2021-01-15 17:22:05 +0530

    Added SimpleCPP files

--- Programming Languages/SimpleCPP/5star.cpp
+++ Programming Languages/SimpleCPP/5star.cpp
@@ -5,11 +5,7 @@ main_program{
     repeat(5){
         forward(100);
         right(135);
-        wait(2.5);
+        wait(5);
     }
-    left(135);
-    forward(100);
-    wait(2.5);
-    right(135);
-    wait(5);
+    wait(50);
 }
