f3ac5ddd3299d6659b3e23a9fd09f24851654ac1
Aman <aakashaman1770@gmail.com>
2021-01-22 22:28:48 +0530

    Modified CPP

---
+++ Programming Languages/SimpleCPP/Z.cpp
@@ -0,0 +1,20 @@
+#include <simplecpp>
+
+main_program{
+    turtleSim();
+
+repeat(2){
+		repeat(3){
+			cout<<"b";
+			repeat(2){
+				cout<<"c";
+			}
+		}
+		repeat(3){
+				cout<<"a";
+				repeat(2){
+					cout<<"a";
+				}
+		}
+}
+}
