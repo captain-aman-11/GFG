8b8ada89d1869d0f32a269146b76e6ea16630b3c
Aman <aakashaman1770@gmail.com>
2021-02-26 01:41:21 +0530

    Modified Files

--- Programming Languages/OOPs/class_Object.cpp
+++ Programming Languages/OOPs/class_Object.cpp
@@ -5,6 +5,9 @@ using namespace std;
 // -> class keyword is used to define a car
 class Car
 {
+private:
+    int price;
+
 public:
     int Number;
     int Model_No;
@@ -15,6 +18,26 @@ public:
         cout << "Name: " << name << endl;
         cout << "Model_No: " << Model_No << endl;
         cout << "Number: " << Number << endl;
+        cout << "Price: " << price << endl;
+    }
+
+    //Setter function for price
+    void set_price(int p)
+    {
+        if (p > 100)
+        {
+            price = p;
+        }
+        else
+        {
+            price = 110;
+        }
+    }
+
+    //Getter function for price
+    int get_price()
+    {
+        return price;
     }
 };
 
@@ -27,8 +50,10 @@ int main()
     c.Number = 20;
     c.Model_No = 01;
     c.name = "BMW";
+    c.set_price(1);
 
     c.print();
+    // cout << c.get_price() << endl;
     cout << sizeof(c) << endl;
 }
 
